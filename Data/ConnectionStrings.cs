5b3978fe803d65d4cd5a118c5ae35151c94ac349
Camilo Aguilar <camiloaguilar1@yahoo.com>
2014-11-06 18:02:28 -0600

    updating project

--- Data/ConnectionStrings.cs
+++ Data/ConnectionStrings.cs
@@ -6,23 +6,19 @@ namespace Data
     {
         public static string Get()
         {
-            var local = ConfigurationManager.ConnectionStrings["local"].ToString();
-
-            //var remote = (ConfigurationManager.ConnectionStrings["remote"].ToString());
-
-            //var production = (ConfigurationManager.ConnectionStrings["production"].ToString());
+            
 
             var environment = (ConfigurationManager.AppSettings["Environment"] ?? "").ToLower();
-            var connectionStringToUse = local;
+            var connectionStringToUse = string.Empty;
 
-
-            if (environment == "qa" || environment == "remote")
+            switch (environment)
             {
-                connectionStringToUse = ConfigurationManager.ConnectionStrings["remote"].ToString();
-            }
-            else if (environment == "production")
-            {
-                connectionStringToUse = string.Empty;
+                case "remote":
+                    connectionStringToUse = ConfigurationManager.ConnectionStrings["remote"].ToString();
+                    break;
+                case "local":
+                    connectionStringToUse = ConfigurationManager.ConnectionStrings["local"].ToString();
+                    break;
             }
 
             return connectionStringToUse;
